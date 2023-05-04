f11aa3a17fb1b2907ec87b40cdd28bf7b5e68d6d
Felipe PCFELIPE-2023-W10-WSL <felipelullio@gmail.com>
2023-05-04 02:19:37 -0300

    version 2 of legenda-video TOP FIX
    da maneira que estava dependia de ter 4 opcoes para selecionar de legenda, pois estava usando irmao do li para selecionar

--- legenda-video.js
+++ legenda-video.js
@@ -1,19 +1,41 @@
-	// clicar no legenda
 {
+		// clicar no legenda
 	document.querySelectorAll('[aria-label="Legendas"]')[0].parentElement.parentElement.click();
 	console.log("click legend icon")
-	
+	var optionDesligado;
+    var optionIngles;
 			// selecionar desligado ou ingles
-	let selectionsAtive = document.querySelectorAll('[aria-checked="true"]');
-	selectionsAtive.forEach(val => {
-		if(val.textContent.includes("Desligado")){
-			// selecionar ingles
-			document.querySelectorAll('[aria-checked="true"')[1].closest('li').nextElementSibling.nextElementSibling.firstChild.click();
-			console.log("legenda: ingles selecionado")
-		}else if(val.textContent.includes("Ingl")){
-			// selecionar desligado
-				document.querySelectorAll('[aria-checked="true"')[1].closest('li').previousSibling.previousSibling.firstChild.firstChild.click();
-				console.log("legenda: desabilitado selecionado")
-		}
-	})
+	var selectionsAtive = document.querySelectorAll('[aria-checked="true"]');
+    var allOptions = document.querySelectorAll('ul[aria-label="Legendas"] li');
+    allOptions.forEach(val => {
+        if(val.textContent.includes('Ingl')){
+            optionIngles = val;
+        }else if(val.textContent.includes('Deslig')){
+            optionDesligado = val;
+        }
+    })
+    if(optionDesligado.firstChild.getAttribute('aria-checked') == 'true'){
+        optionIngles.firstChild.firstChild.click();
+    }else{
+        optionDesligado.firstChild.firstChild.click();
+    }
+
+
+
+	// document.querySelectorAll('[aria-label="Legendas"]')[0].parentElement.parentElement.click();
+	// console.log("click legend icon")
+	
+	// 		// selecionar desligado ou ingles
+	// let selectionsAtive = document.querySelectorAll('[aria-checked="true"]');
+	// selectionsAtive.forEach(val => {
+	// 	if(val.textContent.includes("Desligado")){
+	// 		// selecionar ingles
+	// 		document.querySelectorAll('[aria-checked="true"')[1].closest('li').nextElementSibling.nextElementSibling.firstChild.click();
+	// 		console.log("legenda: ingles selecionado")
+	// 	}else if(val.textContent.includes("Ingl")){
+	// 		// selecionar desligado
+	// 			document.querySelectorAll('[aria-checked="true"')[1].closest('li').previousSibling.previousSibling.firstChild.firstChild.click();
+	// 			console.log("legenda: desabilitado selecionado")
+	// 	}
+	// })
 }
